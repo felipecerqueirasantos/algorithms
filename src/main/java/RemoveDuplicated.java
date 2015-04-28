23db9f008c7a8ddff56420b4790e506fbd0c399a
Felipe Cerqueira dos Santos <felipe.cerqueira@druid.com.br>
2015-04-28 14:28:14 -0300

    Added

---
+++ src/main/java/RemoveDuplicated.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by sky on 28/04/15.
+ */
+public class RemoveDuplicated {
+    public static void main(String[] args) throws Exception {
+        Reader in = new FileReader(args[0]);
+        BufferedReader br = new BufferedReader(in);
+
+        Writer out = new FileWriter(args[1]);
+        BufferedWriter writer = new BufferedWriter(out);
+
+        String line;
+        Set<String> set = new HashSet<>();
+        while ((line = br.readLine()) != null) {
+            String[] arr = line.split(",");
+            // arr[0] = id
+            // arr[1] = msisdn
+
+            if (set.contains(arr[1])) {
+                String s = "DELETE from tb_mailing_ta_ivr_carga where ID_MAILING_IVR_REG = " + arr[0] + ";\r\n";
+                System.out.print(s);
+                writer.write(s);
+                writer.write("COMMIT;\r\n");
+            } else {
+                set.add(arr[1]);
+            }
+        }
+
+        br.close();
+        in.close();
+
+        writer.close();
+        out.close();
+    }
+}
